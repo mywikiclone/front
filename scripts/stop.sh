e14995ac27b95e8a3aafc99e9e8871d223df3fae
dong3058 <hwangdonggeun70@gmail.com>
2024-10-24 11:55:43 +0900

    fir stop.sh

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -2,18 +2,18 @@
 
 # 프로젝트 경로
 PROJECT_ROOT="/home/ec2-user/front"
-JAR_FILE="$PROJECT_ROOT/mywikifront.next"
+#JAR_FILE="$PROJECT_ROOT/mywikifront.next"
 
 # 각종 로그 경로
 DEPLOY_LOG="$PROJECT_ROOT/deploy.log"
 
 TIME_NOW=$(date +'%Y-%m-%d %H:%M:%S')
 
-CURRENT_PID=$(pgrep -f $JAR_FILE)
-
+#CURRENT_PID=$(pgrep -f $JAR_FILE)
+CURRENT_PID=$(lsof -t -i :3000)
 if [ -z $CURRENT_PID ]; then
     echo "[$TIME_NOW] > 실행중인 애플리케이션이 없습니다." >> $DEPLOY_LOG
 else
     echo "[$TIME_NOW] > 실행중인 애플리케이션을 종료합니다. PID = $CURRENT_PID" >> $DEPLOY_LOG
-    kill -15 $CURRENT_PID
+    kill -9 $CURRENT_PID
 fi
