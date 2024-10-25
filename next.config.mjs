b7da2eea901bac2ec25ad03a890c7abc1cbeec0d
dong3058 <hwangdonggeun70@gmail.com>
2024-10-25 19:17:24 +0900

    test

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,31 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    serverActions: true,
-  },headers: [
-    {
-      key: 'Cache-Control',
-      value: 'no-cache, no-store, must-revalidate',
-    },
-  ],
+  /*webpack:(config,{isServer})=>{
+    if(!isServer){
+      config.output.filename="[name]-[contenthash].js";
+      config.output.chunkFilename="[name]-[contenthash].js";
 
+      config.optimization.splitChunks={
+        chunks:'async',
+        cacheGroups:{
+          default:false,
+          vendors:false,
+          common:{
+            name:"common",
+            chunks:'all',
+            minChunks:2,
+            reuseExistingChunk:true,
+            enforce:true,
+          }
+        }
+      }
+    }
+
+    return config;
+  },*/
 
     reactStrictMode: false,
+   
 };
 
 export default nextConfig;
