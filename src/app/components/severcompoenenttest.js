ca48d29e634aca6ddbb69722831d2078c00f28ab
dong3058 <hwangdonggeun70@gmail.com>
2024-11-09 20:02:36 +0900

    test

--- src/app/components/severcompoenenttest.js
+++ src/app/components/severcompoenenttest.js
@@ -3,7 +3,7 @@ const RealTime2=async ()=>{
 
 //서버컴포넌트는 절대적경로만 필요ㅗ 서버에서 실행되니까
     const back_end_url=process.env.NEXT_PUBLIC_BACK_END_URL
-   let data=await fetch("http://ec2-43-201-46-244.ap-northeast-2.compute.amazonaws.com/realtime",{
+   let data=await fetch("http://ec2-13-125-63-133.ap-northeast-2.compute.amazonaws.com/realtime",{
             method:'GET',
             //
             next:{revalidate:60,dynamic:"auto"},
